9b12fe72f57f7c6d5d4212f6fec8d8b44ac2e67b
Vytautas <vytautasjaz@gamil.com>
2020-09-24 15:26:38 +0300

    v1

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/pages/Footer/Footer';
+import Home from './components/pages/HomePage/Home';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
